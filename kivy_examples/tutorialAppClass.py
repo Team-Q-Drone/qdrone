5613758a2730ab1e2165d494202c406efcbb6818
ryantheengineer <ryan.larson.wa@gmail.com>
2019-10-17 14:11:37 -0600

    Changed to hello button

--- kivy_examples/tutorialAppClass.py
+++ kivy_examples/tutorialAppClass.py
@@ -1,7 +1,11 @@
 from kivy.app import App
+from kivy.uix.button import Button
 
 class TutorialApp(App):
-    pass
+    def build(self):
+        return Button(text='Hello!',
+                        background_color=(0,0,1,1),
+                        font_size=150)
 
 if __name__ == '__main__':
     TutorialApp().run()
