33d4724d261da91779e83615aa8a68ff72a9ecb3
ryantheengineer <ryan.larson.wa@gmail.com>
2019-10-17 14:15:51 -0600

    Hello as a movable label using scatter defaults

--- kivy_examples/tutorialAppClass.py
+++ kivy_examples/tutorialAppClass.py
@@ -1,11 +1,20 @@
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.scatter import Scatter
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+
 
 class TutorialApp(App):
     def build(self):
-        return Button(text='Hello!',
-                        background_color=(0,0,1,1),
-                        font_size=150)
+        f = FloatLayout()
+        s = Scatter()
+        l = Label(text='Hello!',
+                    font_size=150)
+
+        f.add_widget(s)
+        s.add_widget(l)
+        return f
+
 
 if __name__ == '__main__':
     TutorialApp().run()
