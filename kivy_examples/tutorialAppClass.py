a676db5f36ae2f209f9f7abea7264f955d906ffc
ryantheengineer <ryan.larson.wa@gmail.com>
2019-10-17 14:41:43 -0600

    Added text input and box layout. Text input is on the right

--- kivy_examples/tutorialAppClass.py
+++ kivy_examples/tutorialAppClass.py
@@ -1,19 +1,28 @@
 from kivy.app import App
+
 from kivy.uix.scatter import Scatter
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
 
 
 class TutorialApp(App):
     def build(self):
+        b = BoxLayout()
+        t = TextInput(font_size=150)
+
         f = FloatLayout()
         s = Scatter()
         l = Label(text='Hello!',
                     font_size=150)
 
+
         f.add_widget(s)
         s.add_widget(l)
-        return f
+        b.add_widget(f)
+        b.add_widget(t)
+        return b
 
 
 if __name__ == '__main__':
