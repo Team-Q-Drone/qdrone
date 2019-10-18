0d416bcf51f36a6a52b207c5a48aed57e96f0809
ryantheengineer <ryan.larson.wa@gmail.com>
2019-10-18 09:43:15 -0600

    Updating before branch merge back to dev.

--- kivy_examples/pong_game/main.py
+++ kivy_examples/pong_game/main.py
@@ -1,11 +1,27 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import (
+    NumericProperty, ReferenceListProperty, ObjectProperty
+)
 from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty()
+    velocity = ReferenceListProperty(velocity_x,velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
 
 
 class PongGame(Widget):
     ball = ObjectProperty(None)
+
+    def serve_ball(self):
+        self.ball.center = self.center
+        self.ball.velocity = Vector(4, 0).rotate(randint(0,360))
 
     def update(self, dt):
         self.ball.move()
@@ -18,30 +34,13 @@ class PongGame(Widget):
         if (self.ball.x < 0) or (self.ball.right > self.width):
             self.ball.velocity_x *= -1
 
-class PongBall(Widget):
-
-    # velocity of the ball on the x and y axis
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-
-    # referencelist property so we can use ball.velocity as
-    # a shorthand, just like e.g. w.pos for w.x and w.y
-    velocity = ReferenceListProperty(velocity_x,velocity_y)
-
-    # ''move'' function will move the ball one step. This will be called in
-    # equal intervals to animate the ball
-
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
-
 
 class PongApp(App):
     def build(self):
         game = PongGame()
+        game.serve_ball()
         Clock.schedule_interval(game.update, 1.0/60.0)
         return game
-
-
 
 
 if __name__ == '__main__':
